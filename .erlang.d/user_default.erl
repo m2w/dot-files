6a298ae0496a3d6fbc52dfc1d868f60f8c3a5325
Moritz <moritz@tibidat.com>
2014-03-22 15:23:51 +0100

    Replaces loads of tabs with spaces.

--- .erlang.d/user_default.erl
+++ .erlang.d/user_default.erl
@@ -54,14 +54,13 @@ debug(Mod, Fun) ->
 dbg_safe(Mod, Fun, Lim) ->
     dbg:start(),
     dbg:tracer(process,
-	       {fun (_, N) when N >= Lim  ->
-			dbg:stop_clear();
-		    (Msg, N) ->
-			io:format("~p~n", [Msg]),
-			N+1
-		end,
-		0
-	       }),
+               {fun (_, N) when N >= Lim  ->
+                        dbg:stop_clear();
+                    (Msg, N) ->
+                        io:format("~p~n", [Msg]),
+                        N+1
+                end,
+                0}),
     dbg:tpl(Mod, Fun, '_', [{'_', [], [{return_trace}]}]),
     dbg:p(all, c).
 
@@ -78,73 +77,73 @@ mm() ->
 
 modified_modules() ->
     [M || {M, _} <- code:all_loaded(),
-	  module_modified(M) == true].
+          module_modified(M) == true].
 
 module_modified(Module) ->
     case code:is_loaded(Module) of
-	{file, preloaded} ->
-	    false;
-	{file, Path} ->
-	    CompileOpts =
-		proplists:get_value(compile, Module:module_info()),
-	    CompileTime = proplists:get_value(time, CompileOpts),
-	    Src = proplists:get_value(source, CompileOpts),
-	    module_modified(Path, CompileTime, Src);
-	_ ->
-	    false
+        {file, preloaded} ->
+            false;
+        {file, Path} ->
+            CompileOpts =
+                proplists:get_value(compile, Module:module_info()),
+            CompileTime = proplists:get_value(time, CompileOpts),
+            Src = proplists:get_value(source, CompileOpts),
+            module_modified(Path, CompileTime, Src);
+        _ ->
+            false
     end.
 
 module_modified(Path, PrevCompileTime, PrevSrc) ->
     case find_module_file(Path) of
-	false ->
-	    false;
-	ModPath ->
-	    case beam_lib:chunks(ModPath, ["CInf"]) of
-		{ok, {_, [{_, CB}]}} ->
-		    CompileOpts =  binary_to_term(CB),
-		    CompileTime = proplists:get_value(time,
-						      CompileOpts),
-		    Src = proplists:get_value(source, CompileOpts),
-		    not (CompileTime == PrevCompileTime)
-			and (Src == PrevSrc);
-		_ ->
-		    false
-	    end
+        false ->
+            false;
+        ModPath ->
+            case beam_lib:chunks(ModPath, ["CInf"]) of
+                {ok, {_, [{_, CB}]}} ->
+                    CompileOpts =  binary_to_term(CB),
+                    CompileTime = proplists:get_value(time,
+                                                      CompileOpts),
+                    Src = proplists:get_value(source, CompileOpts),
+                    not (CompileTime == PrevCompileTime)
+                        and (Src == PrevSrc);
+                _ ->
+                    false
+            end
     end.
 
 find_module_file(Path) ->
     case file:read_file_info(Path) of
-	{ok, _} ->
-	    Path;
-	_ ->
-	    case code:where_is_file(filename:basename(Path)) of
-		non_existing ->
-		    false;
-		NewPath ->
-		    NewPath
-	    end
+        {ok, _} ->
+            Path;
+        _ ->
+            case code:where_is_file(filename:basename(Path)) of
+                non_existing ->
+                    false;
+                NewPath ->
+                    NewPath
+            end
     end.
 
 la() ->
     FiltZip = lists:filter(
-		fun({_Mod, Path}) when is_list(Path) ->
-			case string:str(Path, "/kernel-") +
-			    string:str(Path, "/stdlib-") of
-			    0 -> true;
-			    _ -> false
-			end;
-		   (_) -> false
-		end,
-		code:all_loaded()),
+                fun({_Mod, Path}) when is_list(Path) ->
+                        case string:str(Path, "/kernel-") +
+                            string:str(Path, "/stdlib-") of
+                            0 -> true;
+                            _ -> false
+                        end;
+                   (_) -> false
+                end,
+                code:all_loaded()),
     {Ms, _} = lists:unzip(FiltZip),
     lists:foldl(fun(M, Acc) ->
-			case shell_default:l(M) of
-			    {error, _} ->
-				Acc;
-			    _ ->
-				[M|Acc]
-			end
-		end, [], Ms).
+                        case shell_default:l(M) of
+                            {error, _} ->
+                                Acc;
+                            _ ->
+                                [M|Acc]
+                        end
+                end, [], Ms).
 
 %%--------------------------------------------------------------------
 %% Add minimal documentation for the custom functions
